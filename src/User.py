caf50d1d7afba43cad53f0ba71b2e50a3ffc8d86
Ibrahim Itani <I.itani2002@gmail.com>
2024-07-01 19:01:15 +0300

    for class integration the duplication detection function was integrated as a method within the User class

--- src/User.py
+++ src/User.py
@@ -16,26 +16,27 @@ def getUserName(id):
 # Although every user will get a unique ID so no key error shall occur
 # Yet the user might register a user having the exact same profile data as an existing one
 # This might be a mistake by the user, so a warning will be raised waiting for user confirmation  
-def detectPossibleUserDuplication(name,bio,profilePic):
-    with open(UsersDBPath,'r') as file:
-        usersData = json.load(file)
-        if usersData:
-            for user in usersData.values():
-                if user['name'] == name and user['bio'] == bio and user['profile_picture'] == profilePic:
-                    print("A possible duplication has been detected a user with same name, bio, and profile picture already exits")
-                    print("Do you want to continue registering, ",end="")
-                    choice = input("(y/n)?")
-                    while choice!="y" and choice!="n":
-                        print("Invalid Choice, try again!")
-                        choice = input("(y/n)?")
-                    if choice == "y":
-                        return True
-                    else:
-                        return False
+
     
 class User:
     #initially the userID will be None to force calling the generateID function
     nextUserID = None
+    def detectPossibleUserDuplication(name,bio,profilePic):
+        with open(UsersDBPath,'r') as file:
+            usersData = json.load(file)
+            if usersData:
+                for user in usersData.values():
+                    if user['name'] == name and user['bio'] == bio and user['profile_picture'] == profilePic:
+                        print("A possible duplication has been detected a user with same name, bio, and profile picture already exits")
+                        print("Do you want to continue registering, ",end="")
+                        choice = input("(y/n)?")
+                        while choice!="y" and choice!="n":
+                            print("Invalid Choice, try again!")
+                            choice = input("(y/n)?")
+                        if choice == "y":
+                            return True
+                        else:
+                            return False
     #this function will read from the UsersDb.json file and fetch the highest ID in the DB, then sets an new ID incremented by 1
     def generateUserID():
         try:
@@ -53,7 +54,7 @@ class User:
                   
     def __init__(self,name,bio,profilePic,birthYear,interests=None):
         #To generate a unique ID for each created user
-        
+        if User.detectPossibleUserDuplication(name,bio,profilePic):
             if User.nextUserID is None:
                 self.userId = User.generateUserID()
             self.name = name
@@ -64,7 +65,11 @@ class User:
             self.interests = interests if interests else []
             #ensures no duplicate friendship is stored
             self.friends = set()
-        
+            self.addToDb()
+            print(f"{self.name} has been registered successfully")
+        else:
+            print("Your Registration attempt has been cancelled!")
+            return
     
     def addFriendByID(self,friendId):
         if friendId in self.friends:
@@ -100,26 +105,21 @@ def registerUser(name,bio,profilePic,birthYear,interests):
     #validate that bio is a string
     #validate that the profilePic has the extension .jpg or .png in it
     #validate that birth year contains only digits, of length 4 digits and between 1940-2006 as a value
-    detect = detectPossibleUserDuplication(name,bio,profilePic)
-    if detect is True:
-        if type(name) == str and len(name.split(" "))>=2 and type(bio) == str and (".jpg" in profilePic or ".png" in profilePic) and  1939<int(birthYear)<=2006:
-            newUser = User(name,bio,profilePic,birthYear,interests)
-        #after successful registration of the user, automatically saved to the DB
-            newUser.addToDb()
-            print(f"{name} has been registered successfully")
-            return newUser
-        elif type(name) != str or len(name.split(" "))<2:
-            print("Invalid Name, make sure it is at least of two words and a string")
-        elif type(bio) != str:
-            print("Invalid bio, make sure it is a string")
-        elif ".jpg" not in profilePic and ".png" not in profilePic:
-            print("invalid type of profile pic, only jpg or png are accepted!")
-        elif not 1939<int(birthYear)<=2006:
-            print("Invalid birth year, make sure it is between 1940 and 2006")
-        return 
-    else:
-        print("Your Registration attempt has been cancelled!")
-        return
+    if type(name) == str and len(name.split(" "))>=2 and type(bio) == str and (".jpg" in profilePic or ".png" in profilePic) and  1939<int(birthYear)<=2006:
+        newUser = User(name,bio,profilePic,birthYear,interests)
+    #after successful registration of the user, automatically saved to the DB
+        return newUser
+    elif type(name) != str or len(name.split(" "))<2:
+        print("Invalid Name, make sure it is at least of two words and a string")
+    elif type(bio) != str:
+        print("Invalid bio, make sure it is a string")
+    elif ".jpg" not in profilePic and ".png" not in profilePic:
+        print("invalid type of profile pic, only jpg or png are accepted!")
+    elif not 1939<int(birthYear)<=2006:
+        print("Invalid birth year, make sure it is between 1940 and 2006")
+    return 
+   
+       
         
 
 newUser = registerUser("John Wick","Graphic Designer, loves coffee","john_profile.jpg",1964,None)
