f774439c76838e05abe2ab70865ed1633dfde946
Ibrahim Itani <I.itani2002@gmail.com>
2024-07-01 17:55:48 +0300

    developed a getUserName function to fetch the name of the user depending on its ID. Will be beneficial in data retrieval from the json file

--- src/User.py
+++ src/User.py
@@ -1,1 +1,16 @@
 #  Defines the User class with attributes such as user ID, name, list of friends, and other relevant information.
+import os
+import json
+UsersDBPath = os.path.join('Data','USersDb.json')
+
+def getUserName(id):
+    with open(UsersDBPath,'r') as file:
+        usersData = json.load(file)
+        user = usersData.get(str(id))
+        if user:
+            return True, user.get('name')
+        else:
+            return False , f"User with ID({id}) is not found"
+
+        
+    
