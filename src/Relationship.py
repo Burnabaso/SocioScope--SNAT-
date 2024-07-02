d34f26931106c2ce6288fc7b94b64d9838fb8473
Ibrahim Itani <I.itani2002@gmail.com>
2024-07-02 21:10:57 +0300

    created the feature o adding and removing a friend

--- src/Relationship.py
+++ src/Relationship.py
@@ -1,1 +1,31 @@
 # Manages relationships between users, such as adding or removing friendships or following relationships.
+from RandomRepeatedFunctionalities import *
+
+def addFriendByID(userId,friendId):
+        usersData = loadUsers()
+        result, message = getUserName(friendId)
+        if str(friendId) in usersData[str(userId)].get('friends',[]):
+            print(f"{message} is already friend with User {userId}!")
+            return
+        if result:
+            usersData[str(userId)].get('friends',[]).append(str(friendId))
+            print(f"Added user {message} as a friend")
+            updateUsersDB(usersData)
+        else:
+            print(f"{message}")
+
+def removeFriendByID(userId,friendId):
+    usersData = loadUsers()
+    result, message = getUserName(friendId)
+    if str(friendId) not in usersData[str(userId)].get('friends',[]):
+        print(f"{message} is already not a friend with User {userId}!")
+        return
+    else:
+        if result:
+            usersData[str(userId)].get('friends',[]).remove(str(friendId))
+            print(f"Removed user {message} as a friend")
+            updateUsersDB(usersData)
+        else:
+            print(f"{message}")
+
+removeFriendByID(1,3)
