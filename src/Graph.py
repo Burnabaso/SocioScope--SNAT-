787e1835551f3932f427fd9694ac9436515e4be8
Ibrahim Itani <I.itani2002@gmail.com>
2024-07-07 12:30:56 +0300

    developed a getEdges function that returns the edges list needed to develop the graph

--- src/Graph.py
+++ src/Graph.py
@@ -1,3 +1,16 @@
 # Contains the Graph class for representing the social network using an appropriate data structure (e.g., adjacency list or matrix).
-from RandomRepeatedFunctionalities import *
+from Relationship import getFriendsList
+from RandomRepeatedFunctionalities import loadUsers
+def getEdges():
+   
+    edges = []
+    usersData = loadUsers()
+    for k in usersData.keys():
+        for n in getFriendsList(k):
+            edgeSet = []
+            edgeSet.append(int(k))
+            edgeSet.append(int(n))
+            edges.append(tuple(edgeSet))
+    print(edges)
 
+getEdges()
